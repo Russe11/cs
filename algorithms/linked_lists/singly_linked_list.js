de5977f08122467095ee997d7f7db598bb6312e3
Russell Lewis <russlewis@gmail.com>
2017-01-04 15:23:59 -0500

    Single LinkedList stubs

---
+++ algorithms/linked_lists/singly_linked_list.js
@@ -0,0 +1,38 @@
+
+function Node(data) {
+
+};
+
+function SinglyList() =>{
+    this._length = 0;
+    this.head = null;
+}
+
+SinglyList.prototype.append = (value) => {
+
+};
+
+
+SinglyList.prototype.prepend = (value) => {
+
+};
+
+SinglyList.prototype.remove = (position) => {
+
+}
+
+SinglyList.prototype.print = () => {
+
+}
+
+SinglyList.prototype.isEmpty = () => {
+
+}
+
+SinglyList.prototype.size = () => {
+
+}
+
+SinglyList.prototype.contains = (value) => {
+
+}
